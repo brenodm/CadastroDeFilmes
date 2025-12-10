1731341fc12716ad0cd77fa07a2fe7acc1207548
unknown <breno.maciel1337@gmail.com>
2025-12-09 23:44:05 -0400

    first commit

---
+++ src/main/java/dev/Java10x/Cadastro/de/Filmes/CadastroDeFilmesApplication.java
@@ -0,0 +1,13 @@
+package dev.Java10x.Cadastro.de.Filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeFilmesApplication.class, args);
+	}
+
+}
