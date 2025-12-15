285420e01712882ded76c941466d060cb96de54d
unknown <breno.maciel1337@gmail.com>
2025-12-15 03:39:39 -0400

    Fix - Consertado a Controller

--- src/main/java/dev/Java10x/Cadastro/de/Filmes/controller/FilmesController.java
+++ src/main/java/dev/Java10x/Cadastro/de/Filmes/controller/FilmesController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestControllergit
+@RestController
 @RequestMapping
 public class FilmesController {
 
